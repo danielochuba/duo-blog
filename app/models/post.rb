2adc54ab19cce419805a8f65a9aad437e2109331
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-08 16:58:31 +0100

    fix extra spacing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-  
+
   # Method to return the 5 most recent comments for a given post
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
