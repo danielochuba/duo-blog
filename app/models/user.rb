2484651b68220f09f7c4c853b5d648127fac141d
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-09 02:21:24 +0100

    validate post_count as integer and >= 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-
+  validates :post_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(limit = 3)
     authored_posts.order(created_at: :desc).limit(limit)
