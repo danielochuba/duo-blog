ad67678b2b130c7e053bd0298372650f2ff77418
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-14 23:54:08 +0100

    add inor styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  # get '/home', to: 'home#index'
-  root 'home#index'
+  root 'users#index'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
