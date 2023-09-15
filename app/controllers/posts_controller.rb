f97c63d53ce87fbcb2784cd064de9ddc9bb84a40
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-15 12:00:36 +0100

    remove commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @user_posts = Post.where(author_id: @user.id)
-    # @posts_comments = Comment.where(user_id: )
+
   end
 
   def show
