a4fc94080d6ec15b4281bc047a3e17c5af852f96
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-22 17:41:54 +0100

    fix sign_in and sign out issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
-  def current_user
-    @current_user ||= User.first
-  end
-
-  helper_method :current_user
 
   protect_from_forgery with: :exception
 
