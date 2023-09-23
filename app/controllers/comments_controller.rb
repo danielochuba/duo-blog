3158c22d099e25cc67108dfadd2c7b7f395d7568
Daniel Ochuba <danielochuba78@gmail.com>
2023-09-23 22:02:26 +0100

    add api for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
+
+  def index
+    post = Post.find(params[:post_id])
+    @comments = post.comments
+    render json: @comments
+  end
+
   def new
     @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
@@ -12,10 +19,11 @@ class CommentsController < ApplicationController
     @comment.user = @user
 
     if @comment.save
+      render json: @comment, status: :created
       flash[:success] = 'Comment created successfully'
     else
+      render json: { errors: @comment.errors.full_messages}, status: :unprocessable_entity
       flash[:error] = 'Comment not created'
-      puts @comment.errors.full_messages.inspect
     end
 
     redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
@@ -25,7 +33,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
     flash[:success] = 'Comment deleted successfully'
-    redirect_to
+    redirect_to user_post_path(user_id: @comment.post.author_id, id: @comment.post.id)
   end
 
   private
